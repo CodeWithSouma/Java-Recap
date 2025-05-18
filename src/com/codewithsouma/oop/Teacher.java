76db8e6414e25e072cf28f16a778228199cbd035
CodeWithSouma <souma.hitech@gmail.com>
2025-05-18 10:47:44 +0530

    added a method

--- src/com/codewithsouma/oop/Teacher.java
+++ src/com/codewithsouma/oop/Teacher.java
@@ -5,4 +5,11 @@ public class Teacher {
      String gender;
      long phone;
 
+     public void displayInformation(){
+         System.out.println("Name: "+name);
+         System.out.println("Gender: "+gender);
+         System.out.println("Phone: "+phone);
+         System.out.println();
+     }
+
 }
--- src/com/codewithsouma/oop/Test.java
+++ src/com/codewithsouma/oop/Test.java
@@ -7,18 +7,21 @@ public class Test {
         teacher.gender = "Male";
         teacher.phone = 8768454982L;
 
-        System.out.println("Name: "+teacher.name);
-        System.out.println("Gender: "+teacher.gender);
-        System.out.println("Phone: "+teacher.phone);
+//        System.out.println("Name: "+teacher.name);
+//        System.out.println("Gender: "+teacher.gender);
+//        System.out.println("Phone: "+teacher.phone);
+        teacher.displayInformation();
 
         Teacher teacher2 = new Teacher();
         teacher2.name = "Soumik Nandi";
         teacher2.gender = "Male";
         teacher2.phone = 7076922914L;
 
-        System.out.println("\nName: "+teacher2.name);
-        System.out.println("Gender: "+teacher2.gender);
-        System.out.println("Phone: "+teacher2.phone);
+        teacher2.displayInformation();
+
+//        System.out.println("\nName: "+teacher2.name);
+//        System.out.println("Gender: "+teacher2.gender);
+//        System.out.println("Phone: "+teacher2.phone);
 
     }
 }
