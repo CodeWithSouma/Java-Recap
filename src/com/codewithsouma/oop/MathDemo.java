69927a8e090a1c6ceea11dbcae99727f710e11f3
CodeWithSouma <souma.hitech@gmail.com>
2025-05-24 14:04:02 +0530

    complete math class

---
+++ src/com/codewithsouma/oop/MathDemo.java
@@ -0,0 +1,17 @@
+package com.codewithsouma.oop;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-3.4));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(1));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.max(12, 5));
+        System.out.println(Math.min(5, 3));
+        System.out.println(Math.ceil(3.5));
+        System.out.println(Math.floor(3.2));
+
+    }
+}
